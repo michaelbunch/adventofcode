196f178a60fd297028ef9ac09a2feba633980a00
Michael Bunch <michael.bunch@eltoro.com>
2023-12-01 17:23:59 -0500

    day 1 puzzle 2 solution

--- day1puzzle1.go
+++ day1puzzle1.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -17,19 +18,48 @@ func main() {
 	defer file.Close()
 
 	sum := 0
+
+	words := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		matcher := regexp.MustCompile("[^0-9.]")
-		onlyDigits := string(matcher.ReplaceAll([]byte(scanner.Text()), []byte("")))
+		text := scanner.Text()
 
-		first := onlyDigits[:1]
-		last := first
-		if len(onlyDigits) > 1 {
-			last = onlyDigits[len(onlyDigits)-1:]
+		// Search the line for both numeric and text numbers. Don't forget about overlap numbers.
+		matcher := regexp.MustCompile("[^0-9.]")
+		number := ""
+		for digit := range text {
+			subString := text[digit:]
+
+			if !matcher.Match([]byte(subString[:1])) {
+				number = number + subString[:1]
+				continue
+			}
+
+			for i, word := range words {
+				if strings.Index(subString, word) == 0 {
+					number = number + strconv.Itoa(i)
+				}
+			}
 		}
 
-		coordinates, _ := strconv.Atoi(first + last)
+		// Find the first and last digits of the string
+		first := number[:1]
+		last := first
+		if len(number) > 1 {
+			last = number[len(number)-1:]
+		}
+
+		// Make the final coordinate
+		coordinates, err := strconv.Atoi(fmt.Sprintf("%s%s", first, last))
+		if err != nil {
+			fmt.Println("can't build coordinates")
+			os.Exit(1)
+		}
+
+		// Add to the total
 		sum += coordinates
+
+		// fmt.Println(scanner.Text(), number, first, last, coordinates, sum)
 	}
 
 	fmt.Println(sum)
